dcc2fc5cfb138b23d271a18ec4f3f650b903fb62
Mr-Incredibl3 <144457856+Mr-Incredibl3@users.noreply.github.com>
2023-09-09 00:29:33 +1000

    Update CTQuickScript.sh

--- CTQuickScript.sh
+++ CTQuickScript.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #Written by L Rouse to do simple tasks for Cyber Taipan competition
+if [ "$EUID" -ne 0 ]
+  then echo "run this crap with sudo"
+  exit
+fi
 echo updating...
 apt-get install update
 echo upgrading...
