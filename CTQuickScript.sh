39e0ae73b9c2cc949f01192212e0b1c9ed92a1f4
Mr-Incredibl3 <144457856+Mr-Incredibl3@users.noreply.github.com>
2023-09-09 17:39:01 +1000

    Update CTQuickScript.sh

--- CTQuickScript.sh
+++ CTQuickScript.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 #Written by L Rouse to do simple tasks for Cyber Taipan competition
 if [ "$EUID" -ne 0 ]
-  then echo "run this crap with sudo"
+  then echo "run this crap with sudo plz"
+  exit
+fi
+read -p "HAVE YOU DONE FORENSICS? (y/n)"
+if [[ $REPLY =~ ^[Nn]$ ]]
+  then echo "Do the forensics before running this script unless otherwise instructed"
+  exit
+elif [[ $REPLY =~ ^[Yy]$ ]]
+  then #commmences script
+else
+  then echo "not y or n, ending script"
   exit
 fi
 echo updating...
