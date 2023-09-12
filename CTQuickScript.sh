957ddf516e749fd2a978547fdee7ca16ef056b2f
Mr-Incredibl3 <144457856+Mr-Incredibl3@users.noreply.github.com>
2023-09-12 11:46:30 +1000

    User Prompts
    Script now prompts user for confirmation before running

--- CTQuickScript.sh
+++ CTQuickScript.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 #Written by L Rouse to do simple tasks for Cyber Taipan competition
-if [ "$EUID" -ne 0 ]
+if [ "$EUID" -ne 0 ] #checks if script is being run as super user
   then echo "run this crap with sudo plz"
   exit
 fi
-read -p "HAVE YOU DONE FORENSICS? (y/n)"
-if [[ $REPLY =~ ^[Nn]$ ]]
-  then echo "Do the forensics before running this script unless otherwise instructed"
-  exit
-elif [[ $REPLY =~ ^[Yy]$ ]]
-  then #commmences script
+read -p "HAVE YOU DONE FORENSICS? (y/n)" forensics #confirms that the user wishes to run the script
+if [[ $forensics = y ]] ; then #commmences script
+echo "beginning script..."
+elif [[ $forensics = n ]] ; then #stops script
+echo "Do the forensics before running this script unless otherwise instructed"
+exit
 else
-  then echo "not y or n, ending script"
-  exit
+echo "not y or n, ending script" #stops script due to invalid input
+exit
 fi
-echo updating...
+echo "updating..."
 apt-get install update
-echo upgrading...
+echo "upgrading..."
 apt-get upgrade
-echo updates complete
+echo "updates complete"
+
